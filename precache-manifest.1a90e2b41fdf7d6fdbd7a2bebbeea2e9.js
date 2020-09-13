7f924eaa5a2d55f02739301d6f9eb697b964a6bd
Guilherme Rodrigues <guilodron7@gmail.com>
2020-09-13 14:34:13 -0300

    Updates

---
+++ precache-manifest.1a90e2b41fdf7d6fdbd7a2bebbeea2e9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ce1ff4fc1794fd4f5ea1e1ebeb7e2e",
+    "url": "/artzy/index.html"
+  },
+  {
+    "revision": "f147054f6eae3684793d",
+    "url": "/artzy/static/js/2.5a946358.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/artzy/static/js/2.5a946358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde3d746a08cf891c234",
+    "url": "/artzy/static/js/main.bc668dc7.chunk.js"
+  },
+  {
+    "revision": "d1f67bbca3925fcadee7",
+    "url": "/artzy/static/js/runtime-main.eae3ae09.js"
+  },
+  {
+    "revision": "9a6863ba5b562ead3c97fbe111ca50f4",
+    "url": "/artzy/static/media/1.9a6863ba.jpg"
+  },
+  {
+    "revision": "aa67f4079cd634ce1a8033cb613fe9ce",
+    "url": "/artzy/static/media/2.aa67f407.jpg"
+  },
+  {
+    "revision": "561d42c9b8b50119381973643f5a5e47",
+    "url": "/artzy/static/media/3.561d42c9.jpg"
+  },
+  {
+    "revision": "df2745b7690019562185e483e3fa59c5",
+    "url": "/artzy/static/media/4.df2745b7.jpg"
+  },
+  {
+    "revision": "bbdfb1b81443ca8260807650704453ae",
+    "url": "/artzy/static/media/5.bbdfb1b8.jpg"
+  },
+  {
+    "revision": "5176e794ce890e6adc4c5dc6f5104abe",
+    "url": "/artzy/static/media/6.5176e794.jpg"
+  },
+  {
+    "revision": "a6741c4b89e773b001fbcf22b2fb2a98",
+    "url": "/artzy/static/media/7.a6741c4b.jpg"
+  },
+  {
+    "revision": "b79fc8e57dcb30e07d25f222bf8c92ca",
+    "url": "/artzy/static/media/8.b79fc8e5.jpg"
+  }
+]);
